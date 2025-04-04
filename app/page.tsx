2f2f1d13138dda4e43c7dbb40ec03bf6a9fa5733
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-05 00:45:05 +0300

    explore optimization technics

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,45 @@
+"use client";
 import Image from "next/image";
+import house from "@/public/images/house.jpg";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import { useState } from "react";
+import dynamic from "next/dynamic";
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
+
+export default function Home() {
+  // const session = await getServerSession(authOptions);
+  const [isVisible, setVisible] = useState(false);
   return (
     <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+      {/* <h1>Hello {session && <span>{session.user!.name}</span>}</h1> */}
+      {/* <Link href="/users">Users</Link> */}
+      {/* <ProductCard /> */}
+      <h1>Hello world</h1>
+      <button className="btn btn-primary" onClick={() => setVisible(true)}>
+        Show
+      </button>
+      {isVisible && <HeavyComponent />}
     </main>
   );
+  // return (
+  //   <main className="relative h-screen">
+  //     {/* <Image src={house} alt="House" /> */}
+  //     <Image
+  //       src="https://ghiblicollection.com/cdn/shop/files/Totoro_PDP_Banner.png"
+  //       alt="Totoro"
+  //       fill
+  //       className="object-cover"
+  //       sizes="(max-width: 480) 100vw, (max-width: 768px) 50vw, 33vw"
+  //       quality={100}
+  //       priority
+  //     />
+  //   </main>
+  // );
 }
