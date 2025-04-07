c10072c2de3fc96335e85bfc20bb7bcccedab04c
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-07 15:05:08 +0300

    add issue create form

---
+++ app/api/issues/route.ts
@@ -0,0 +1,21 @@
+import { createIssueSchema } from "@/app/validationSchemas";
+import { prisma } from "@/prisma/client";
+import { NextRouter } from "next/router";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
+}
