aba339b60428f6b3fe98553e34661d47858f0169
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-07 20:40:18 +0300

    add next auth

---
+++ app/auth/authOptions.ts
@@ -0,0 +1,17 @@
+import { prisma } from "@/prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+};
+
+export default authOptions;
