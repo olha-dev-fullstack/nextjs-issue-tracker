0c3a9efe886beaa8e211a6410e57ba8c494074e5
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-08 13:48:21 +0300

    align home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,36 @@
-import Image from "next/image";
+import { prisma } from "@/prisma/client";
+import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import { Flex, Grid } from "@radix-ui/themes";
+import IssueChart from "./IssueChart";
+import { Metadata } from "next";
 
-export default function Home() {
-  return <div>Hello World</div>;
+export default async function Home() {
+  // return <LatestIssues />;
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
+        <LatestIssues />
+    </Grid>
+  );
 }
+
+export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "Issue Tracker - Dashboard",
+  description: "View a summary of project issues",
+};
