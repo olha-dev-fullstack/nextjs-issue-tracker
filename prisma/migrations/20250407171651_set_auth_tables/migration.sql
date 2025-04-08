d0208ce2e8bbb5bf1d0a8a94efa7f0151d04935d
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-08 15:03:38 +0300

    add cloud database

--- prisma/migrations/20250407171651_set_auth_tables/migration.sql
+++ prisma/migrations/20250407171651_set_auth_tables/migration.sql
@@ -58,7 +58,8 @@ CREATE TABLE `VerificationToken` (
     `token` VARCHAR(191) NOT NULL,
     `expires` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`)
+    UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`),
+    PRIMARY KEY (`identifier`, `token`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
